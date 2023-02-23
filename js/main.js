873d85e3fd0a9c739b9a12cc1e1b2ef3acb07373
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-02-23 18:00:24 +0100

    Implementazione con math.floor

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ const geButton = document.querySelector('#informazioni');
 
  geButton.addEventListener('click', 
     function () {
+        let utente = document.getElementById("utente").value;
         let km = document.getElementById("km").value;
-        let anni
+        let anni=parseInt(document.getElementById("promo").value)
         const prezzo = 0.21;
         let somma = km * prezzo
         
@@ -13,7 +14,7 @@ const geButton = document.querySelector('#informazioni');
             let sconto = somma * 20 / 100
             somma= somma - sconto
             somma = somma.toFixed(2);
-            document.getElementById("sconto1").innerHTML="Sconto ragazzi under 18 : " + sconto.toFixed(2) + "€";
+            
         
             
             
@@ -21,7 +22,7 @@ const geButton = document.querySelector('#informazioni');
             let sconto = somma * 40 / 100
             somma= somma - sconto
             somma = somma.toFixed(2)  ;
-            document.getElementById("sconto1").innerHTML="Sconto over 65 : " + sconto.toFixed(2) + "€" ;
+            
            
         
         }else{
@@ -30,6 +31,7 @@ const geButton = document.querySelector('#informazioni');
         }
         
         document.getElementById("totale").innerHTML="Il totale del prezzo del biglietto è: " + somma + "€";
+        document.getElementById("nome_utente").innerHTML= utente;
      
     }
 );
