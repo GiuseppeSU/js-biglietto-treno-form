797a91c46defeb352772e894cbdc19033ab241db
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-02-23 19:33:23 +0100

    Fine programma e inizio css

--- js/main.js
+++ js/main.js
@@ -2,37 +2,44 @@
 
 const geButton = document.querySelector('#informazioni');
 
- geButton.addEventListener('click', 
+geButton.addEventListener('click',
     function () {
         let utente = document.getElementById("utente").value;
         let km = document.getElementById("km").value;
-        let anni=parseInt(document.getElementById("promo").value)
+        let anni = parseInt(document.getElementById("promo").value)
         const prezzo = 0.21;
         let somma = km * prezzo
-        
-        if(anni < 18 ){
+        let numeroCasuale = Math.floor(Math.random()*10) +1;
+        const numeroCp = Math.floor((Math.random() * 90001)) + 10000;
+
+       
+
+        if (anni < 18) {
             let sconto = somma * 20 / 100
-            somma= somma - sconto
+            somma = somma - sconto
             somma = somma.toFixed(2);
-            
-        
-            
-            
-        } else if (anni >= 66 ) {
+
+
+
+
+        } else if (anni >= 66) {
             let sconto = somma * 40 / 100
-            somma= somma - sconto
-            somma = somma.toFixed(2)  ;
-            
-           
-        
-        }else{
-            
+            somma = somma - sconto
+            somma = somma.toFixed(2);
+
+
+
+        } else {
+
             somma = somma.toFixed(2);
         }
-        
-        document.getElementById("totale").innerHTML="Il totale del prezzo del biglietto è: " + somma + "€";
-        document.getElementById("nome_utente").innerHTML= utente;
-     
+
+        document.getElementById("totale").innerHTML =  somma + "€";
+        document.getElementById("nome_utente").innerHTML = utente;
+        document.getElementById("offerta").innerHTML =  anni;
+        document.getElementById("carrozza").innerHTML =  numeroCasuale  ;
+        document.getElementById("code").innerHTML =  numeroCp  ;
+
     }
 );
 
